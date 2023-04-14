a1f6e5687c768220136114378ed4b349f13fd07b
dotaii <dohuutai98@gmail.com>
2023-04-14 22:50:15 +0700

    Tabs UI

---
+++ Lesson_5_TabsUI/main.js
@@ -0,0 +1,27 @@
+    const $ = document.querySelector.bind(document)
+    const $$ = document.querySelectorAll.bind(document)
+
+    const tabs = $$('.tab-item')
+    const panes = $$('.tab-pane')
+
+    const tabActive = $('.tab-item.active');
+    const tabLine = $('.tabs .tab-line')
+
+    
+    tabLine.style.left = tabActive.offsetLeft + 'px'
+    tabLine.style.width = tabActive.offsetWidth + 'px'
+
+
+    tabs.forEach((tab, index) => {
+        const pane = panes[index];
+        tab.onclick = function () {
+            $('.tab-item.active').classList.remove('active')
+            $('.tab-pane.active').classList.remove('active')
+
+            tabLine.style.left = this.offsetLeft + 'px'
+            tabLine.style.width = this.offsetWidth + 'px'
+
+            this.classList.add('active')
+            pane.classList.add('active')
+        }
+})
